638b8098937db3238fed43fca0e79eae89311e62
Vivek Kashyap <vivekyeskashyap@gmail.com>
2020-06-29 20:58:09 +0530

    Nothing much

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -4,7 +4,7 @@ import './main.css';
 class Recipe extends Component {
     render() {
         return this.props.data.map((item, index) => (
-            <div className="cardsize card" key={index} style={{width: "30rem", height: '700px', overflow: 'scroll'}}>
+            <div className="cardsize card" key={index} style={{maxWidth: "30rem", maxHeight: '700px', overflow: 'scroll'}}>
                 <img style={{maxHeight: '200px'}} src={item.img} className="card-img-top" alt="Item type" />
                 <div className="card-body" alt="item body">
                     <h5 className="card-title">{item.name}</h5>
